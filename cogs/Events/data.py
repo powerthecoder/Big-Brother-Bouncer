565d80f84d84cda11e4a6a3a991b208d7e4423c7
powerthecoder <68101457+powerthecoder@users.noreply.github.com>
2021-05-11 13:14:54 -0600

    Create data.py

---
+++ cogs/Events/data.py
@@ -0,0 +1,39 @@
+import os
+import sys
+import time
+import random
+import discord
+from discord.ext import commands
+from discord.ext import tasks
+from discord import Member
+from discord.ext.commands import has_permissions
+from discord.ext.commands import has_role
+from discord.ext.commands import MissingPermissions
+from discord.utils import find
+from discord.utils import get
+import asyncio
+import json
+import datetime
+
+class Main(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+        self.console_log = client.get_channel(791852159519686666)
+    
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        
+
+    @commands.Cog.listener()
+    async def on_message_delete(self, message):
+        embed=discord.Embed(title="Deleted Message", description=f"**Author**: {message.author.mention} \n**Message**: {message.content}")
+        await console_log.send(embed=embed)
+        now = datetime.now()
+        current_time = now.strftime("%Y-%m-%d %H:%M:%S")
+        log = open("deleted_messages_log.txt", "w")
+        log.write(f"{current_time}  |  {message.author.mention}  |  {message.content}")
+        log.close()
+
+
+def setup(client):
+    client.add_cog(Main(client))
