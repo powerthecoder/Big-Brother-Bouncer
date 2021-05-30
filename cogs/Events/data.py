76c280ddeadf61330b5e85aa5ea1d9ac741f4265
powerthecoder <68101457+powerthecoder@users.noreply.github.com>
2021-05-29 23:50:49 -0600

    Update data.py

--- cogs/Events/data.py
+++ cogs/Events/data.py
@@ -19,10 +19,6 @@ class Main(commands.Cog):
     def __init__(self, client):
         self.client = client
         self.console_log = client.get_channel(791852159519686666)
-    
-    @commands.Cog.listener()
-    async def on_message(self, message):
-        
 
     @commands.Cog.listener()
     async def on_message_delete(self, message):
