5eaa9db0737bb7e4f1b8903378a64cedc1030cf0
powerthecoder <68101457+powerthecoder@users.noreply.github.com>
2021-05-29 23:54:51 -0600

    Update help.py

--- cogs/Commands/help.py
+++ cogs/Commands/help.py
@@ -12,9 +12,9 @@ class Main(commands.Cog):
     
     @commands.command()
     async def help(self, ctx):
-        embed=discord.Embed(title="Help Menu",description="Prefix - \n<> Required \n[] Optional")
+        embed=discord.Embed(title="Help Menu",description="Prefix . \n<> Required \n[] Optional")
         embed.add_field(name=".help", value="This Menu", inline=False)
-        embed.add_field(name=".choice <arg1,arg2,arg3,arg4>", value="Bot randomly chooses a choice. Up to 4 choices", inline=False)
+        embed.add_field(name=".choice <arg1> <arg2> [arg3] [arg4]", value="Bot randomly chooses a choice. Up to 4 choices", inline=False)
         embed.add_field(name=".flip", value="Bot flips a coin", inline=False)
         await ctx.send(embed=embed)
 
